b5cbce89f197467903f7d912aeecb4bbc9181278
neonoxd <neonoxd14@gmail.com>
2017-11-25 17:54:36 +0100

    welcome page template

--- src/main/java/com/alkfejl/hu/Beadando/NavigationController.java
+++ src/main/java/com/alkfejl/hu/Beadando/NavigationController.java
@@ -1,14 +1,15 @@
 package com.alkfejl.hu.Beadando;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class NavigationController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello world!";
+        return "welcome";
     }
  
 }
